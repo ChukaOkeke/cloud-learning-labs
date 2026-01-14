ca7e990e57542343b6d9004b92bc6947f4bcd8f0
ChukaOkeke <chukaookeke@gmail.com>
2026-01-14 17:07:10 +0100

    initial commit: set up cloud learning roadmap repo

---
+++ k8s/k8s-web-hello/index.mjs
@@ -0,0 +1,15 @@
+import express from "express";
+import os from "os";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  const hello_message = `Hello from the ${os.hostname()}!`;
+  console.log(hello_message);
+  res.send(hello_message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
